77de0f52520840da22c7958030d88a6e4a6e454c
Trotteriniku <stefanozidda3@gmail.com>
2023-10-12 16:50:50 +0200

    adjust const variabile

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
-const employeMail = ['example@1.com','example@2.com','example@3.com','example@4.com'];
-const mailValue =document.getElementById('email').value;
+
+
 const mailCeckBtn =document.querySelector('button');
-console.log(mailValue);
 
 mailCeckBtn.addEventListener('click',function () {
+const employeMail = ['example@1.com','example@2.com','example@3.com','example@4.com'];
+const mailValue = document.getElementById('email').value;
+
+
         if (employeMail.includes(mailValue)) {
-            alert=('Perfetto benvenuto')
-            console.log(mailValue);
+            
         } else{
-            alert=('mi spiace ma non riconosciamo l'')
+           
         }
         
    
