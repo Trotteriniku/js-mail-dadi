0204f0bdccc97a51f6f08774573a64282cc2c634
Trotteriniku <stefanozidda3@gmail.com>
2023-10-12 15:57:12 +0200

    email exercise

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 const employeMail = ['example@1.com','example@2.com','example@3.com','example@4.com'];
-const mailValue =document.getElementById('email'.value);
-
+const mailValue =document.getElementById('email').value;
 const mailCeckBtn =document.querySelector('button');
-mailCeckBtn.addEventListener('click',function () {
-    for(let i = 0; i < employeMail.length; i++ ){
-        if () {
-            
-        }
-    }
+console.log(mailValue);
 
+mailCeckBtn.addEventListener('click',function () {
+        if (employeMail.includes(mailValue)) {
+            alert=('Perfetto benvenuto')
+            console.log(mailValue);
+        } else{
+            alert=('mi spiace ma non riconosciamo l'')
+        }
+        
+   
 })
+   
