e44dc893713d73d7cd851f2e426626dc095be53b
Trotteriniku <stefanozidda3@gmail.com>
2023-10-12 17:24:09 +0200

    email cecker + alert element

--- js/script.js
+++ js/script.js
@@ -5,14 +5,18 @@ const mailCeckBtn =document.querySelector('button');
 mailCeckBtn.addEventListener('click',function () {
 const employeMail = ['example@1.com','example@2.com','example@3.com','example@4.com'];
 const mailValue = document.getElementById('email').value;
-
-
+const alertText =document.getElementById('alertText')
+let alert =document.querySelector('.alert')
+    
         if (employeMail.includes(mailValue)) {
-            
+           alert.classList.add('alert-success')
+            alertText.innerText= 'Bentornato' 
         } else{
-           
+            alertText.innerText= 'Mi spiace ma non riusciamo a trovare la tua email nei nostri reggistri. Riprova con un\'altra mail'
+           alert.classList.add('alert-warning')
         }
         
-   
+        alert.classList.remove('d-none')
+        
 })
    
